0bd64f8115299b3f8855c9bbbefb65b92ab90f3e
mitliao <mitliao@gmail.com>
2019-01-16 16:29:40 +0800

    Initial commit

--- config.sh
+++ config.sh
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "     Disable HTC Boot Sound    "
   ui_print "*******************************"
 }
 
@@ -52,17 +52,8 @@ print_modname() {
 # List all directories you want to directly replace in the system
 # Check the documentations for more info about how Magic Mount works, and why you need this
 
-# This is an example
 REPLACE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
-# Construct your own list here, it will override the example above
-# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
-REPLACE="
+/system/customize/resource/HTC_Sense5_Boot.mp3
 "
 
 ##########################################################################################
